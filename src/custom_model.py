7245e9b7a98a72bcdfdd2f3ae9f5f7b4ddbc3328
Emily Huang <144239497+EmilyHuang111@users.noreply.github.com>
2024-12-09 23:44:05 -0500

    Update custom_model.py

--- src/custom_model.py
+++ src/custom_model.py
@@ -3,10 +3,10 @@ from q_learning import QLearning  # Assuming QLearning is your model class
 
 
 class CUSTOM_AI_MODEL:
-    def __init__(self, model_path="trained_models/tetris_2000_state_dict.pth"):
+    def __init__(self, model_path="trained_models/tetris_2000"):
         # Load the pre-trained model
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-       self.model = QLearning()
+        self.model = QLearning()
         self.model.load_state_dict(torch.load(model_path, map_location=self.device))
         self.model.to(self.device)
         self.model.eval()
