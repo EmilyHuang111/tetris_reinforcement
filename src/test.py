8a00b5c2d73ecae3d92e7b21152cc35f328b7777
Emily Huang <144239497+EmilyHuang111@users.noreply.github.com>
2024-12-10 01:54:03 -0500

    Update test.py

--- src/test.py
+++ src/test.py
@@ -26,7 +26,7 @@ def test(opt):
 
     # Initialize and load the model
     model = QLearning()  # Ensure this matches the training model
-    model.load_state_dict(torch.load("{}/tetris".format(opt.model_save_path), map_location=device))
+    model.load_state_dict(torch.load("{}/tetris_2000_state_dict".format(opt.model_save_path), map_location=device))
     model.to(device)
     model.eval()
 
